1f9cc20c3ea500b7f2db6ae1dd014385b954e5b7
KhetrabasiReddy <reddykhetrabasi@gmail.com>
2025-04-15 12:38:05 +0530

    added the wallpaper

--- src/components/AppIcon.jsx
+++ src/components/AppIcon.jsx
@@ -20,12 +20,12 @@ import React from 'react';
 const AppIcon = ({ name, image, onClick }) => {
   return (
     <div
-      className="rounded-xl h-16 w-16 p-2 bg-gray-100 dark:bg-gray-700 shadow-inner flex flex-col items-center justify-center cursor-pointer hover:scale-105 transition-transform"
+      className="rounded-xl h-16 w-16 p-2  shadow-inner flex flex-col items-center justify-center cursor-pointer hover:scale-105 transition-transform"
       title={name}
       onClick={onClick}
     >
       <img src={image} alt={name} className="h-8 w-8 object-contain mb-1" />
-      <span className="text-[10px] text-center text-black dark:text-white font-medium">{name}</span>
+      <span className="text-[10px] text-center text-white font-medium">{name}</span>
     </div>
   );
 };
